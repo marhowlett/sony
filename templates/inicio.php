4118b0a9d1cbbaff917a3a55fb4fd633f0a8ce74
Maria del Mar Ramonell <marimar_r2003@hotmail.com>
2018-10-08 21:52:00 -0500

    comienzo2

--- templates/inicio.php
+++ templates/inicio.php
@@ -4,13 +4,66 @@
 ?>
 
 <section id="banner" class="template-section padding-50">
+   <div class="contenedor">
     <div class="grid">
         <p>Hoy mismo</p>
         <h2>Reparaciones garantizadas</h2>
         <p>De Pantallas LED, LCD, Smart y Plasma <br>Todas marcas</p>
         <a href="#" class="btn amarillo">CONTACTAR</a>
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/marcas.png" alt="Logos">
     </div>
+     </div>
 </section>
+<section id="banner" class="template-section">
+  <div class="contenedor">
+   <div class="grid">
+       <h3>Te ofrecemos</h3>
+       <div class="col3">
+           <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/honradez.png" alt="Honradez">
+           <p>Honradez y seriedad absoluta</p>
+       </div>
+       <div class="col3">
+           <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/utilizamos.png" alt="utlizamos">
+           <p>Utilizamos refacciones originales
+</p>
+       </div>
+       <div class="col3">
+           <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/servicio.png" alt="servicio">
+           <p>Servicio a Domicilio</p>
+       </div>
+       <div class="col3">
+           <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/tv.png" alt="TV">
+           <p>En taller, la revisión es gratis </p>
+       </div>
+       <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/paypal.png" alt="Paypal">
+   </div>
+     </div>
+</section>
+<section id="reparacion" class="template-section">
+  <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/colores.png" alt="Colores">
+   <div class="contenedor">
+   
+    <div class="grid">
+        
+        <p>Reparación GARANTIZADA de</p>
+        <h2>HORNOS DE MICROONDAS DE TODAS LAS MARCAS</h2>
+        <p>en Cuernavaca y sus alrededores. <br>
+¡Llame, nosotros vamos!</p>
+   <a href="#" class="btn amarillo">CONTACTAR</a>
+    </div>
+     </div>
+</section>
+<section id="tarjetas" class="template-section">
+   <div class="contenedor">
+    <div class="grid">
+        <h3>venta de tarjetas </h3>
+        <p>Tenemos venta de tarjetas para pantallas de todas marcas.
+Envíanos la foto de la tarjeta que necesitas por <span>WhatsApp (744) 195 1898 </span> o por correo <span>serviciosonyhitachi@gmail.com</span> y te diremos si la tenemos disponible.</p>
+   <a href="#" class="btn amarillo">CONTACTAR</a>
+    </div>
+     </div>
+</section>
+
 
 
  <?php 
